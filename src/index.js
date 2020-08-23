feaa2c96887e732bb4ca3c4a9eeabf66d5903265
SilviaMW87 <SilviaMW87@users.noreply.github.com>
2020-08-23 18:37:47 +0200

    updated index.html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement =document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp;
+}
+
 let apiKey = "aaa7ba886d5c66918cc62a40c92a74723";
 let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=London,uk&appid=${apiKey}`;
 
 console.log(apiUrl);
+axios.get(url).then(displayTemperature);
+
+function showForecast(response) {
+    console.log(response);
+}
+
